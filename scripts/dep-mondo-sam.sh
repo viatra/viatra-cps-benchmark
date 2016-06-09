23f23645ae110a7f725ff40712eedbcdfbc16b56
Abel Hegedus <abel.hegedus@incquerylabs.com>
2016-06-09 18:45:36 +0200

    [releng] removed mondo-sam from build

--- scripts/dep-mondo-sam.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd "$( cd "$( dirname "$0" )" && pwd )/.."
-
-rm -rf mondo-sam
-git clone --branch 0.1-maintenance git@github.com:FTSRG/mondo-sam.git mondo-sam
--- scripts/update.sh
+++ scripts/update.sh
@@ -3,4 +3,3 @@
 cd "$( cd "$( dirname "$0" )" && pwd )"
 
 ./dep-cps-demo.sh
-./dep-mondo-sam.sh
