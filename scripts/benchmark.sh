cbf9ff11c96ccdc6a160e0a55b50562387aa7a38
Abel Hegedus <abel.hegedus@incquerylabs.com>
2016-06-10 09:47:56 +0200

    update scripts to allow benchmark on Jenkins

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-cd "$( cd "$( dirname "$0" )" && pwd )"
+cd "$( cd "$( dirname "$0" )" && pwd )/.."
 
 rm -rf benchmark
 
+mkdir benchmark
 cp -r releng/com.incquerylabs.examples.cps.rcpapplication.headless.product/target/products/com.incquerylabs.examples.cps.benchmark.app/linux/gtk/x86_64/. benchmark/eclipse
 cp scripts/run_linux.py benchmark
 cp scripts/convert_results.sh benchmark
