78a286fc0c9cd17756a9edef215f4f5cbed914ab
Abel Hegedus <abel.hegedus@incquerylabs.com>
2016-06-10 15:00:49 +0200

    split benchmark and reporting scripts

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -7,12 +7,8 @@ mkdir benchmark
 cp -r releng/com.incquerylabs.examples.cps.rcpapplication.headless.product/target/products/com.incquerylabs.examples.cps.benchmark.app/linux/gtk/x86_64/. benchmark/eclipse
 cp scripts/run_linux.py benchmark
 cp scripts/convert_results.sh benchmark
-cp scripts/config.json benchmark
-cp scripts/report.sh benchmark
 
 cd benchmark
 python3 run_linux.py
 chmod +x convert_results.sh
 ./convert_results.sh
-chmod +x report.sh
-mkdir diagrams
--- scripts/report.sh
+++ scripts/report.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-cd "$( cd "$( dirname "$0" )" && pwd )"
+cd "$( cd "$( dirname "$0" )" && pwd )/.."
 
+cd benchmark
+rm -rf diagrams
+mkdir diagrams
 python3 ${WORKSPACE}/mondo-sam/reporting/report.py --source ${WORKSPACE}/benchmark/results/results.csv \
---output ${WORKSPACE}/benchmark/diagrams/ --config ${WORKSPACE}/benchmark/config.json
+--output ${WORKSPACE}/benchmark/diagrams/ --config ${WORKSPACE}/scripts/config.json
