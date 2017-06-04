88de876dcf3154926840dbe0962157befd344b98
Ábel Hegedüs <abel.hegedus@gmail.com>
2017-06-04 18:00:20 +0200

    Create dep-mondo-sam.sh

---
+++ scripts/dep-mondo-sam.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd "$( cd "$( dirname "$0" )" && pwd )/.."
+
+# Clone results repository if needed
+if [ -d "mondo-sam" ]; then
+  # Repo exists, update
+  cd mondo-sam
+  git fetch
+  git reset origin/0.1-maintenance --hard
+  cd ..
+else
+  # Clone repo
+  git clone git@github.com:FTSRG/mondo-sam.git mondo-sam
+  git checkout origin/0.1-maintenance
+fi
